01c1bba3c41462039a22e0091d180081a91dd2c9
Bannette <banetaloo3@gmail.com>
2019-06-08 20:34:29 +0300

    changed the font type and modified contact information

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,22 @@ $("#management").click(function(){
     $("#management img").toggle();
    $("#management p").toggle();
 });
+$("#submit").click(function() {
+  var email = $("#email").val();
+  alert(email + " we have received your message. Thank you for subscribing");
+   $(".message")[0].reset();
+});
+});
+
+var key = '1c2089a31a00d28af6cb0e553ed84697-us20';
+var user = document.getElementById('username').value;
+$("button").click(function(event) {
+  event.preventDefault();
+  alert("Dear " + user + ", we have received your message. Thank you for reaching out to us.");
+});
+
+$("button").on('click', function(){
+  $('form').each(function(){
+    this.reset();
+  });
 });
