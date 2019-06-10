b303914bf5028b1e85270287a80a5b9d6e1bbd8d
Bannette <banetaloo3@gmail.com>
2019-06-10 10:08:03 +0300

    added hover effect on each portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,103 @@ $("#management").click(function(){
     $("#management img").toggle();
    $("#management p").toggle();
 });
+
+$(".para").hover(function(){
+  $(".para p").show().css({
+    "position":"absolute",
+    "color": "white",
+    "top": "47.5%",
+    "left": "22%"
+  });
 });
+$(".para").mouseleave(function (){
+  $(".para p").hide();
+});
+
+$(".para1").hover(function(){
+  $(".para1 p").show().css({
+    "position":"absolute",
+    "color": "white",
+    "top": "47.5%",
+    "left": "12%"
+  });
+});
+$(".para1").mouseleave(function (){
+  $(".para1 p").hide();
+});
+
+$(".para2").hover(function(){
+  $(".para2 p").show().css({
+    "position":"absolute",
+    "color": "white",
+    "top": "47.5%",
+    "left": "22%"
+  });
+});
+$(".para2").mouseleave(function (){
+  $(".para2 p").hide();
+});
+
+$(".para3").hover(function(){
+  $(".para3 p").show().css({
+    "position":"absolute",
+    "color": "white",
+    "top": "47.5%",
+    "left": "30%"
+  });
+});
+$(".para3").mouseleave(function (){
+  $(".para3 p").hide();
+});
+$(".para4").hover(function(){
+  $(".para4 p").show().css({
+    "position":"absolute",
+    "color": "white",
+    "top": "50%",
+    "left": "22%"
+  });
+});
+$(".para4").mouseleave(function (){
+  $(".para4 p").hide();
+});
+
+$(".para5").hover(function(){
+  $(".para5 p").show().css({
+    "position":"absolute",
+    "color": "white",
+    "top": "47.5%",
+    "left": "22%"
+  });
+});
+$(".para5").mouseleave(function (){
+  $(".para5 p").hide();
+});
+$(".para6").hover(function(){
+  $(".para6 p").show().css({
+    "position":"absolute",
+    "color": "white",
+    "top": "47.5%",
+    "left": "22%"
+  });
+});
+$(".para6").mouseleave(function (){
+  $(".para6 p").hide();
+});
+
+$(".para7").hover(function(){
+  $(".para7 p").show().css({
+    "position":"absolute",
+    "color": "white",
+    "top": "47.5%",
+    "left": "22%"
+  });
+});
+$(".para7").mouseleave(function (){
+  $(".para7 p").hide();
+});
+});
+
+
 
 $(document).ready(function() {
   $("form#contacts").submit(function(event) {
@@ -29,3 +125,4 @@ $(document).ready(function() {
     }
   });
 });
+
